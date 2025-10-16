a9994d10fd7231f772ded714d9cc630ca7138ff4
tryzz05 <trystanhaigh@icloud.com>
2025-10-16 12:21:50 +1030

    updated pig

--- pig.cpp
+++ pig.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 Pig::Pig(Texture2D pigText){
-    Animal(pigText, "Pig", "Wool", 150, 80, 100);
+    Animal(pigText, "Pig", "Bacon", 150, 80, 100);
 };
 
 void Pig::updateDaily() {
